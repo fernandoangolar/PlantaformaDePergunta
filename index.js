3e76ca8f434741f7047640110daa959172681a22
Fernando Angolar <fernandoangolar581@gmail.com>
2023-11-24 23:35:52 +0100

    Terminei a parte da stilo da página

--- index.js
+++ index.js
@@ -6,30 +6,16 @@ const port = 8080
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome
-    let lang = req.params.lang
-    let exibirMSG = false
-    
-    let produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 14},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4}
-    ]
-
-    res.render("index", {
-        nome: nome,
-        lang, lang,
-        empresa: "Guia do angolar",
-        inscritos: 100000,
-        msg : exibirMSG,
-        produtos : produtos
-    } )
+app.get("/", (req, res) => {
+ 
+    res.render("index")
 } )
+
+app.get("/perguntar", (req, res) => {
+    res.render('perguntar');
+})
 
 app.listen(port, () => {
     console.log("App rodando na porta ", port)
-}) 
+}) ;
+
